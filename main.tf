8f7a796f890ef8095cc5893fa5a3761c45dcd3b4
brajasekharreddy <raj479.mits@gmail.com>
2023-02-22 03:15:39 +0530

    instances for Doc DB cluster

--- main.tf
+++ main.tf
@@ -44,10 +44,26 @@ resource "aws_docdb_cluster" "docdb" {
   skip_final_snapshot    = true
   db_subnet_group_name   = aws_docdb_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.docdb.id]
+  //storage_encrypted      = true
+  //kms_key_id             = data.aws_kms_key.key.arn
 
   tags = merge(
     local.common_tags,
     { Name = "${var.env}-docdb-cluster" }
+  )
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = var.number_of_instances
+  identifier         = "${var.env}-docdb-cluster-instance-${count.index + 1}"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = var.instance_class
+  //storage_encrypted  = true
+  //kms_key_id         = data.aws_kms_key.key.arn
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-docdb-cluster-instance-${count.index + 1}" }
   )
 }
 
