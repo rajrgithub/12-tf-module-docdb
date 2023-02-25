05ed9412f2db22087c1eca04e39351986141009c
brajasekharreddy <raj479.mits@gmail.com>
2023-02-25 16:41:54 +0530

    Added DOC DB parameters imstead of Mango after installing DOCDB with terraform

--- main.tf
+++ main.tf
@@ -67,3 +67,20 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   )
 }
 
+resource "aws_ssm_parameter" "docdb_url_catalogue" {
+  name  = "${var.env}.catalogue.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.DB_ADMIN_USER.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name  = "${var.env}.user.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.DB_ADMIN_USER.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url" {
+  name  = "${var.env}.docdb.DOCDB_URL"
+  type  = "String"
+  value = aws_docdb_cluster.docdb.endpoint
+}
